bd10a60ee574b589cceae83a7809cbcffd6da85d
apoclyps <kyle90adam@hotmail.com>
2023-03-11 10:35:53 +0000

    adds scaffolding for CRUD routes

--- README.md
+++ README.md
@@ -16,8 +16,88 @@ Make a curl request to the `/` route:
 curl http://localhost:8000/ | jq .
 ```
 
-Response: 
+Response:
 
 ```json
 "Hello, world!"
+```
+
+## Endpoints
+
+- GET - List heros
+
+```bash
+ curl http://localhost:8000/heros | jq .
+```
+
+```json
+  {
+    "id": 1,
+    "name": "Clark Kent"
+  },
+  {
+    "id": 2,
+    "name": "Bruce Wayne"
+  }
+]
+```
+
+- GET - show hero
+
+```bash
+curl http://localhost:8000/heros/1 | jq .
+```
+
+```json
+{
+  "email": "clark.kent@dailyplanet.org",
+  "id": 2,
+  "name": "Clark Kent"
+}
+```
+
+- POST - create new
+
+```bash
+ curl -X POST http://localhost:8000/heros -H 'Content-Type: application/json' | jq .
+```
+
+```json
+{
+  "email": "clark.kent@dailyplanet.org",
+  "id": 3,
+  "name": "John Doe"
+}
+```
+
+- PUT - Update existing
+
+```bash
+ curl -X PUT http://localhost:8000/heros/1 -H 'Content-Type: application/json' | jq .
+```
+
+```json
+{
+  "email": "clark.kent@dailyplanet.org",
+  "id": 1,
+  "name": "Clark Kent"
+}
+```
+
+- DELETE - Detel existing
+
+```bash
+ curl -X DELETE http://localhost:8000/heros -I
+```
+
+```bash
+HTTP/1.1 404 Not Found
+content-type: text/html; charset=utf-8
+server: Rocket
+permissions-policy: interest-cohort=()
+x-content-type-options: nosniff
+x-frame-options: SAMEORIGIN
+content-length: 383
+date: Sat, 11 Mar 2023 10:34:26 GMT
+
 ```
--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,53 @@
 #[macro_use]
 extern crate rocket;
 
-use rocket::serde::json::{Value, json};
+use rocket::response::status;
+use rocket::serde::json::{json, Value};
 
 #[get("/")]
-fn hello() -> Value {
-    json!("Hello, world!")
+fn index() -> Value {
+    json!("Weclome to Hero API")
+}
+
+#[get("/heros")]
+fn get_heros() -> Value {
+    json!([{"id": 1, "name": "Clark Kent"}, {"id": 2, "name": "Bruce Wayne"}])
+}
+
+#[get("/heros/<id>")]
+fn view_hero(id: i32) -> Value {
+    json!({"id": id, "name": "Clark Kent", "email": "clark.kent@dailyplanet.org"})
+}
+
+#[post("/heros", format = "json")]
+fn create_hero() -> Value {
+    json!({"id": 3, "name": "John Doe", "email": "clark.kent@dailyplanet.org"})
+}
+
+#[put("/heros/<id>", format = "json")]
+fn update_hero(id: i32) -> Value {
+    json!({"id": id, "name": "Clark Kent", "email": "clark.kent@dailyplanet.org"})
+}
+
+#[delete("/heros/<_id>")]
+fn delete_hero(_id: i32) -> status::NoContent {
+    status::NoContent
 }
 
 #[rocket::main]
 async fn main() {
-    let _ = rocket::build().mount("/", routes![hello]).launch().await;
+    let _ = rocket::build()
+        .mount(
+            "/",
+            routes![
+                index,
+                get_heros,
+                view_hero,
+                create_hero,
+                update_hero,
+                delete_hero
+            ],
+        )
+        .launch()
+        .await;
 }
